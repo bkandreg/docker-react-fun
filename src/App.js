448a3f462bd4fbdf0990f946e807357ae4e460e9
Bhanu Prakash Kandregula <bhanugates@gmail.com>
2019-09-12 19:07:59 -0400

    hola feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bhanu Prakash 
+          Bhanu Prakash Kandregula from New York!!!! Cheers!!!
         </p>
         <a
           className="App-link"
